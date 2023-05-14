0c0a1f347425944b0b9024407d0f82b418a9dc5b
thelocalgodd <vkkpemlie@st.knust.edu.gh>
2023-05-14 19:26:30 +0000

    14/5/23

---
+++ question22.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int firstterm, secondterm;
+int main() {
+    std::cout << "enter first term of sequence: ", std::cin >> firstterm;
+    std::cout << "enter second term of sequence: ", std::cin >> secondterm;
+
+   
+}
